8fb87d68b59dcbf39c84f8c399a6937435059fb4
jasonn1n <blazzer1547777@gmail.com>
2025-05-15 21:04:42 +0500

    g8ю1-4

--- resources/views/admin/logs/index.blade.php
+++ resources/views/admin/logs/index.blade.php
@@ -6,7 +6,7 @@
         <!-- Боковое меню -->
         <x-side-menu />
         <!-- Основной контент -->
-        <div class="col-md-10">
+        <div class="col-md-10" style="margin-top: 30px;">
             <h2>Последние логи</h2>
             <pre style="background:#222;color:#eee;padding:1em;border-radius:8px;max-height:600px;overflow:auto;">
 @foreach(array_slice($logs,0,100) as $line)
